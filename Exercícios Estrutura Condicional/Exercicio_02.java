60c069a01e2ad1fa7ac15954b92e7d814bc8571a
Rafael Vivilechio Carodoso <54628356+Vivilechio@users.noreply.github.com>
2022-02-09 17:03:03 -0300

    Exercícios Estrutura Condicional

---
+++ Exercícios Estrutura Condicional/Exercicio_02.java
@@ -0,0 +1,26 @@
+package exercicios_estrutura_condicional;
+
+import java.util.Scanner;
+
+public class Exercicio_02 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int x;
+
+		System.out.println("Digite o número:");
+		x = sc.nextInt();
+
+		if (x % 2 == 0) {
+			System.out.println("PAR");
+		} else {
+			System.out.println("ÍMPAR");
+
+		}
+
+		sc.close();
+	}
+
+}
