6a0fead8d796b18bc975b51694790f5bb221274a
Rafael Vivilechio Carodoso <54628356+Vivilechio@users.noreply.github.com>
2022-03-17 21:38:10 -0300

    Estrutura Condicional

---
+++ Exercícios Estrutura Condicional/Exemplo_05.java
@@ -0,0 +1,37 @@
+package condicional;
+
+import java.util.Scanner;
+
+public class Exemplo_05 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		String dia;
+		if (x == 1) {
+			dia = "Domingo";
+		} else if (x == 2) {
+			dia = "Segunda";
+		} else if (x == 3) {
+			dia = "Terça";
+		} else if (x == 4) {
+			dia = "Quarta";
+		} else if (x == 5) {
+			dia = "Quinta";
+		} else if (x == 6) {
+			dia = "Sexta";
+		} else if (x == 7) {
+			dia = "Sábado";
+		} else {
+			dia = "Dia Inválido";
+		}
+
+		System.out.println("Dia Selecionado: " + dia);
+
+		sc.close();
+
+	}
+
+}
